3a92f811771ded6e9341347d46139c1cf85f6b0a
e-afzal <essam.afzal@outlook.com>
2023-08-22 19:53:43 +0400

    Agents page: Responsive on Desktop/Mobile

--- app/agents/page.jsx
+++ app/agents/page.jsx
@@ -6,6 +6,7 @@ import styles from "@/public/styles/pages/agents.module.scss";
 
 // COMPONENTS
 import Navbar from "@/app/components/Navbar";
+import Footer from "@/app/components/Footer";
 
 async function getAgents() {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/agents`);
@@ -16,7 +17,7 @@ export default async function Agents() {
   const agents = await getAgents();
   return (
     <>
-      <Navbar paddingTop={0} />
+      <Navbar paddingTop={"2.5rem"} height="10rem" />
       <section id={styles.team}>
         <h1>OUR TEAM</h1>
         <hr />
@@ -42,6 +43,7 @@ export default async function Agents() {
             })}
         </div>
       </section>
+      <Footer marginTop={"5rem"} />
     </>
   );
 }
